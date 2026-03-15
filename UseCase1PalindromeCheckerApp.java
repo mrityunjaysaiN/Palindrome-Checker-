b9310ff62fc188b4343b6e0edefc35060c06b214
mrityunjaysaiN <mrityunjaysai01@gmail.com>
2026-03-15 15:03:46 +0530

    Palindrom

---
+++ UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Palindrome Checker App");
+        System.out.println("Version 1.0");
+        System.out.println("UC1: Application Entry & Welcome Message");
+    }
+}
